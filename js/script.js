4bcc32ffb384d3abfc346dcb53aad1ee218690ee
Andrzej Grabowski <andgrabowski@gmail.com>
2019-01-22 09:04:34 +0100

    Simplify script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var data = [{
         id: 'box1',
         title: 'First box',
@@ -32,37 +31,17 @@ var data = [{
     },
 ];
 
-var boxes = document.getElementsByTagName('div');
-var headerCategories = [];
-var boxCategories = [];
-
 for (var i = 0; i < data.length; i++) {
-    document.write('<div><header>' + data[i].title + '</header>' + data[i].content + '</div>');
-    boxes[i].classList.add('box');
-    boxes[i].id = data[i].id;
-
-};
-
-//znajdź header i wstaw mu klasę 
-const addHeaderClass = function (className) {
-    for (var i = 0; i < boxes.length; i++) {
-        var idCaught = document.getElementById('box' + (i + 1));
-        var headerInBox = idCaught.getElementsByTagName('header');
-        // console.log(headerInBox[i]);
-        headerInBox[i].classList.add(className);
+    var divs = document.createElement('div');
+    var headers = document.createElement('header');
+    divs.id = data[i].id;
+    divs.appendChild(headers);
+    headers.innerHTML = data[i].title;
+    divs.innerHTML += data[i].content;
+    // divs.className = data[i].categories.join(" "); // prosty sposob-rozdzielenie klas
+    for (var j = 0; j < data[i].categories.length; j++) {
+        divs.classList.add(data[i].categories[j]);
     }
-}
-for (var j = 0; j < data.length; j++) {
-    var box = boxes[j];
-    // console.log(box);
-    for (var i = 0; i < data[j].categories.length; i++) {
-        if (data[j].categories[i] !== 'special-header') {
-            // console.log(data[j].categories[i]);
-            box.classList.add(data[j].categories[i]);
-        } else {
-            addHeaderClass(data[j].categories[i]);
-            // addHeaderClass('special-header');
-        }
 
-    }
+    document.body.appendChild(divs);
 }
